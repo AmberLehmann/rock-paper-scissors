907df791f07b20f08b8606feaeb20d52fabae10b
AmberLehmann <132320992+AmberLehmann@users.noreply.github.com>
2023-12-26 04:13:53 -0500

    Added basic UI to Rock, Paper, Scissors
    Added basic button functionality and scoreboard that updates dynamically.

--- main.js
+++ main.js
@@ -1,75 +1,109 @@
 //Best Of 5 Game of Rock, Paper, Scissors
 
-
 let playerWins = 0;
 let computerWins = 0;
 const GAME_OPTIONS = ['ROCK', 'PAPER', 'SCISSORS']
 
-function getPlayerChoice() {
-  return prompt('What is your choice?').toUpperCase();
-}
-
 function getComputerChoice() {
-  let randomInt = Math.floor(Math.random() * 3);
-  return GAME_OPTIONS[randomInt];
+  let randomInt = Math.floor(Math.random() * 3); //Selects index
+  return GAME_OPTIONS[randomInt]; //Selects a option from the array
 }
 
 function playRound (playerSelection, computerSelection) {
   if (playerSelection == computerSelection) { //When both are equal its a tie
+    displayGameStatus(playerSelection, computerSelection, 'tie');
     return 'tie';
   }
   if(playerSelection == 'ROCK' && computerSelection == 'SCISSORS' ||  //One of three winning conditions must be true for a win
      playerSelection == 'SCISSORS' && computerSelection == 'PAPER' ||
      playerSelection == 'PAPER' && computerSelection == 'ROCK') {
     playerWins++;
+    updateScore();
+    displayGameStatus(playerSelection, computerSelection, 'win');
     return 'win';
   }
-  if(playerSelection != 'ROCK' && playerSelection != 'SCISSORS' && playerSelection != 'PAPER') { //User must pick one of the three options
-    return 'invalid';
-  }
   computerWins++;
+  updateScore(); 
+  displayGameStatus(playerSelection, computerSelection, 'loss');
   return 'loss' //If it isn't a win or a tie it must be a loss
 }
 
-
-
 function displayGameStatus(playerChoice, computerChoice, outcome){
-  if (outcome == 'invalid') {return 'Invalid option. Please pick either Rock, Paper or Scissors.'}
-  let display = 'It\'s a ' + outcome + '. ';
-  playerChoice = capitalizeWord(playerChoice)
-  computerChoice = capitalizeWord(computerChoice)
+  let display = '';
+  playerChoice = capitalizeWord(playerChoice);
+  computerChoice = capitalizeWord(computerChoice);
   switch (outcome) {
     case 'tie':
-      display+= 'Both of you picked ' + playerChoice;
+      display+= 'Great minds think alike. Both of you picked ' + playerChoice;
+      changeDisplay(display);
       break;
     case 'win':
-      display+= ''+ playerChoice + ' beats ' + computerChoice + '.'
+      if (playerWins == 5) {
+        displayGameWin();
+        resetGame();
+      }
+      else {
+        display+= 'Good job! '+ playerChoice + ' beats ' + computerChoice + '.'
+        changeDisplay(display);
+      }
       break;
     case 'loss':
-      display+= ''+ computerChoice + ' beats ' + playerChoice + '.'
+      if (computerWins == 5) {
+        displayGameLoss();
+        resetGame();
+      }
+      else {
+        display+= 'Nice try. '+ computerChoice + ' beats ' + playerChoice + '.'
+        changeDisplay(display);
+      }
       break;
     }
   return display;
 }
 
-function displayCurrentScore(){
-  return 'You: ' + playerWins + '. Computer: ' + computerWins; 
+
+function updateScore () {
+  displayPlayerScore[0].textContent = playerWins;
+  displayComputerScore[0].textContent = computerWins;
+}
+function changeDisplay (text) {
+  displayGame[0].textContent = text;
+}
+
+function displayGameWin() {
+  changeDisplay('Congrats! You won 5 times. Select a button to play again.');
+}
+
+function displayGameLoss() {
+  changeDisplay('Oof... You lost 5 times. Select a button to play again.');
+}
+
+function resetGame() {
+  updateScore();
+  playerWins = 0;
+  computerWins = 0;
+
 }
 
 function capitalizeWord (word) {
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
 
+const rockButton = document.getElementsByClassName('rock-btn');
+rockButton[0].addEventListener('click', () => {
+  playRound("ROCK", getComputerChoice());
+})
 
+const scissorsButton = document.getElementsByClassName('scissors-btn');
+scissorsButton[0].addEventListener('click', () => {
+  playRound("SCISSORS", getComputerChoice());
+})
 
-while (playerWins < 5 && computerWins < 5) {
-  let playerChoice = getPlayerChoice();
-  let computerChoice = getComputerChoice();
-  let outcome = playRound(playerChoice, computerChoice);
-  console.log(displayGameStatus(playerChoice, computerChoice, outcome));
-  console.log(displayCurrentScore());
-} 
-if (playerWins == 5) {console.log('Congrats on the win!')}
-else {console.log ('You lost! Nice try.')}
+const paperButton = document.getElementsByClassName('paper-btn');
+paperButton[0].addEventListener('click', () => {
+  playRound("PAPER", getComputerChoice());
+})
 
-
+const displayGame = document.getElementsByClassName('game-status');
+const displayPlayerScore = document.getElementsByClassName('player-score');
+const displayComputerScore = document.getElementsByClassName('cpu-score');
